f93c034b58890af43ef859f1eae7dc5f8faab5d7
daniel.galindo <daniel.galindo@COC02C34J2MD6M.local>
2022-11-29 18:20:39 -0500

    Project basic structure on Django

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Regresion
+
+This project aims to generate a community focused on econometric models.
---
+++ scripts/vars.py
@@ -0,0 +1,31 @@
+from pandas_datareader import wb
+#import pandas as pd
+
+from base.models import Variable, Country
+
+
+def run():
+
+    Variable.objects.all().delete()
+    Country.objects.all().delete()
+
+    df_vars = wb.get_indicators()
+    df_country = wb.get_countries()    
+
+    for var in range(len(df_vars)):
+
+        variable = Variable(
+            var_id = df_vars.iloc[var]['id'],
+            name = df_vars.iloc[var]['name']
+        )
+        variable.save()
+    
+    for count in range(len(df_country)):
+
+        country = Country(
+            name = df_country.iloc[count]['name']
+        )
+        country.save()
+
+if __name__ == '__main__':
+    run()
